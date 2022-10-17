28420d778074e81a4c035699d5e8264447a05291
anandadwipra <anandabiru04@gmail.com>
2022-10-17 09:05:17 +0700

    Initialize

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/monkeycod3/app-golang
+
+go 1.19
+
+require github.com/monkeycod3/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_helo "github.com/monkeycod3/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_helo.Say("Halo major update"))
+	fmt.Println("testing")
+}
